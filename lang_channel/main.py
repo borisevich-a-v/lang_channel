3c661182d704f63c26e2f5841acb31dabdca5a6e
Andrey Borisevich <tinker000@yandex.ru>
2022-10-29 23:18:50 +0300

    write logs into a file

--- lang_channel/main.py
+++ lang_channel/main.py
@@ -10,6 +10,9 @@ from src.google_sheets import registry
 from src.post_post import post_post
 from src.telegram_bot.bot import run_bot
 
+logger.remove()
+logger.configure(handlers=[dict(sink="/logs.log")])
+
 app = FastAPI()
 bot = telegram.Bot(token=settings.tg_bot_token)
 
@@ -28,7 +31,7 @@ async def get_status() -> Dict[str, str]:
 @app.post("/publish_post", responses={200: {"description": "Post posted successfully"}})
 async def publish_post() -> Dict[str, str]:
     logger.info("Request on endpoint `publish_post`")
-    magic_number_min, magic_number_max = 10, 7 * 60
+    magic_number_min, magic_number_max = 10, 3 * 60
     time_to_sleep = randint(magic_number_min, magic_number_max)
     await asyncio.sleep(time_to_sleep)
     await post_post(bot, registry)
