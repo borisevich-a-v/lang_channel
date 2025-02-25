28cff55daf15c196b43921128aa167c491dbbd1a
andrei <andreyborisevichleti@gmail.com>
2025-02-25 22:10:17 +0300

    feat: publish post by schedule

--- lang_channel/__main__.py
+++ lang_channel/__main__.py
@@ -2,6 +2,7 @@ from loguru import logger
 from telegram.ext import ApplicationBuilder
 
 from lang_channel import config, handlers
+from lang_channel.jobs.publish_post import publish_post
 
 HANDLERS = (
     handlers.HELP_COMMAND,
@@ -21,6 +22,9 @@ def main():
     logger.info("Registering handlers...")
     for h in HANDLERS:
         application.add_handler(h)
+
+    logger.info("Adding jobs...")
+    application.job_queue.run_daily(publish_post, time=config.POST_PUBLISHING_TIME)
 
     logger.info("Starting polling...")
     application.run_polling()
--- lang_channel/jobs/publish_post.py
+++ lang_channel/jobs/publish_post.py
@@ -1,24 +1,18 @@
 from loguru import logger
-from telegram import Update
 from telegram.ext import ContextTypes
 
 from lang_channel import config
 from lang_channel.storage.google_sheets import registry
 
 
-async def process_publish_post(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def publish_post(context: ContextTypes.DEFAULT_TYPE):
     logger.info("Publishing a post...")
     post = registry.get_next_post()
-    if not post:
-        await update.message.reply_text("No posts to publish")
+    if post is None:
+        logger.info("Nothing to publish")
         return
-
-    logger.info(f"Post {post.id_} has been downloaded")
     await context.bot.send_photo(chat_id=config.CHANNEL_ID, photo=post.photo, caption=post.text)
     await context.bot.send_voice(chat_id=config.CHANNEL_ID, voice=post.voice)
-    logger.info(f"Post {post.id_} has been published")
-
+    post.publish_count = int(post.publish_count) + 1
     registry.archive_post(post)
-    logger.info(f"Post {post.id_} has been archived")
-
-    await update.message.reply_text(text="Пост опубликован.")
+    logger.info(f"Successfully published the post with id: {post.id_}")
